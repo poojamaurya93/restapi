8889236e724d1c377473055a2f19a6ac392eb1bc
bizzeonline30 <rahul@mcansey.com>
2026-02-18 18:16:18 +0530

    fix media/static

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
    
 ]
 CORS_ALLOW_ALL_ORIGINS = True
